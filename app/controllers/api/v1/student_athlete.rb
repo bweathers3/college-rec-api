1404a06cc301a627f31154426c3085774e49c7e3
Bailey <bailey.g.weathers@gmail.com>
2018-01-01 16:51:01 -0500

    pause 1

--- app/controllers/api/v1/student_athlete.rb
+++ app/controllers/api/v1/student_athlete.rb
@@ -2,8 +2,10 @@ module Api
   module V1
     class StudentAthleteController < ApplicationController
 
+      before_action :set_studentAthlete, only: [:show, :destroy]
 
       def index
+        render json: StudentAthlete.all
       end
 
       def create
@@ -15,6 +17,14 @@ module Api
       def destroy
       end
 
+    private
+      def set_studentAthlete
+        @stdentAthlete = StudentAthlete.find(params[:id])
+      end
+
+      def studentAthlete_params
+        params.permit(:firstName, :middleName, :lastName, :birthdate)
+      end
 
     end
   end
