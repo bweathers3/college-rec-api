74d95aeada6db1a1c8a780520a2e9fb264894019
Bailey <bailey.g.weathers@gmail.com>
2018-01-17 09:49:54 -0500

    Add Country

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180117141242) do
+ActiveRecord::Schema.define(version: 20180117144846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20180117141242) do
     t.text "siblings"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "country"
     t.index ["student_athlete_id"], name: "index_profiles_on_student_athlete_id"
   end
 
